8fe06e6124e4ae9b03d0c61bddbf096a1766005c
Abia Zahir <abiazahir@hotmail.com>
2024-03-20 19:21:31 -0400

    First Revision

--- Tech-Lab-On-Campus/Producer-And-Consumer/consumer/consumer_interface.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/consumer_interface.py
@@ -12,37 +12,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pika
+import os
 
 class mqConsumerInterface:
     def __init__(
         self, binding_key: str, exchange_name: str, queue_name: str
     ) -> None:
         # Save parameters to class variables
-
+        
         # Call setupRMQConnection
+        
         pass
 
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service
-
+        
         # Establish Channel
-
+        
         # Create Queue if not already present
-
+    
         # Create the exchange if not already present
-
+        
         # Bind Binding Key to Queue on the exchange
-
+        
         # Set-up Callback function for receiving messages
+        
         pass
 
     def on_message_callback(
         self, channel, method_frame, header_frame, body
     ) -> None:
         # Acknowledge message
-
+    
         #Print message (The message is contained in the body parameter variable)
-
+        
         pass
 
     def startConsuming(self) -> None:
@@ -55,7 +59,7 @@ class mqConsumerInterface:
         # Print "Closing RMQ connection on destruction"
         
         # Close Channel
-
+        
         # Close Connection
         
         pass
